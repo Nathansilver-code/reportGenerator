c4f1f409ad70bfb044329b7b0d41f982b2618398
Nathan Silver <nathansilver17@gmail.com>
2026-07-29 05:22:00 +0300

    image on a report card

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Teacher(UserMixin, db.Model):
 
     id            = db.Column(db.Integer, primary_key=True)
     full_name     = db.Column(db.String(120), nullable=False)
-    email         = db.Column(db.String(120), unique=True, nullable=False)
+     email         = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     role          = db.Column(db.String(20), nullable=False, default="teacher")
     created_at    = db.Column(db.DateTime, default=datetime.utcnow)
